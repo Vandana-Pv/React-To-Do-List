b6762a758670ca4175ceada15a6f1eaec05c0f26
Vandana-Pv <pv.vandana29@gmail.com>
2020-06-13 13:15:57 +0530

    Completed Add update and delete functions in ToDo List

--- src/App.js
+++ src/App.js
@@ -1,14 +1,95 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-          Hello World !
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newItem: "",
+      list: []
+    };
+  }
+
+  onFormSubmitHandler = e =>{
+    e.preventDefault();
+    this.setState({
+      newItem: "",
+      list: [...this.state.items, this.state.term]
+    })
+  }
+
+  updateInput(key, value) {
+    // update react state
+    this.setState({ [key]: value });
+  }
+
+  addItem() {
+    // create a new item with unique id
+    const newItem = {
+      id: 1 + Math.random(),
+      value: this.state.newItem.slice()
+ 
+    };
+
+    // copy current list of items
+    const list = [...this.state.list];
+
+    // add the new item to the list
+    list.push(newItem);
+
+    // update state with new list, reset the new item input
+    this.setState({
+      list,
+      newItem: ""
+    });
+  }
+
+  deleteItem(id) {
+    // copy current list of items
+    const list = [...this.state.list];
+    // filter out the item being deleted
+    const updatedList = list.filter(item => item.id !== id);
+
+    this.setState({ list: updatedList });
+  }
+
+  render(){
+    return(
+      <div className="App"> 
+      <h1> ToDoList</h1>
+      <form onSubmit={this.onFormSubmitHandler}>
+          <input 
+            type = "text" 
+            placeholder="Type item here" 
+            value={this.state.newItem}
+            onChange = {e =>this.updateInput("newItem",e.target.value)} 
+          />
+          <button
+            onClick={() => this.addItem()}
+            disabled={!this.state.newItem.length}
+          > 
+          <i class="material-icons">add_box</i>
+          </button>       
+      </form>
+          <br>
+          </br>
+          <ul>
+            {this.state.list.map(item => {
+              return (
+                <li key={item.id}>
+                  {item.value}
+                  <button 
+                  onClick={() => this.deleteItem(item.id)}
+                  >
+                  <i class="material-icons">delete</i>
+                  </button>
+                </li>
+              );
+            })}
+          </ul>
+      </div>
+    )
+  }
 }
 
 export default App;
